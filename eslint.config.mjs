6e4e4f2abaaaaa1f88b7f7608fd691c7e47e068b
thanmayi0610 <thanmayithanmayi81@gmail.com>
2025-04-23 11:27:33 +0530

    Refactor ESLint configuration and update Prettier settings for consistency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,23 @@
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
+// import tanstackQuery from "@tanstack/eslint-config-query";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
+
+// const eslintConfig = [
+//   ...tanstackQuery.config["flat/recommended"],
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+// ];
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import tanstackQuery from "@tanstack/eslint-config-query";
-
+import tanstackQuery from "@tanstack/eslint-plugin-query"
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -11,10 +26,11 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...tanstackQuery.config["flat/recommended"],
+...tanstackQuery.config["flat/recommended"],
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
-
-
 export default eslintConfig;
+
+
+
