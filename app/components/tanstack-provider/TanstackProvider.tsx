a430563645ac94f847fdf76a1d61d75b14c2a9f1
thanmayi0610 <thanmayithanmayi81@gmail.com>
2025-04-23 11:15:40 +0530

    Refactor TanstackProvider component for improved readability

--- app/components/tanstack-provider/TanstackProvider.tsx
+++ app/components/tanstack-provider/TanstackProvider.tsx
@@ -1,15 +1,11 @@
-"use client"
+import { tanstackQueryClient } from '@/lib/integrations/tanstack'
+import { QueryClientProvider } from '@tanstack/react-query'
+import React, { PropsWithChildren } from 'react'
+const TanstackProvider = (props: PropsWithChildren) => {
+  return (
+    <QueryClientProvider client={tanstackQueryClient}>
+      {props.children}
+    </QueryClientProvider>
+  );
+};export default TanstackProvider
 
-import { tanstackQueryClient } from "@/lib/integrations/tanstack"
-import { QueryClientProvider } from "@tanstack/react-query"
-import { PropsWithChildren } from "react"
-
-const TanstackProvider = (props:PropsWithChildren) => {
-    return(
-        <QueryClientProvider client={tanstackQueryClient}>
-            {props.children}    
-        </QueryClientProvider>
-    )
-
-}
-export default TanstackProvider
